8f9e374d8c17267393ae2178823c1be17401f131
lizimeow <lizimeowww@gmail.com>
2017-11-24 13:41:57 +0800

    优化代码结构 & 增加在线计数

--- crawl.js
+++ crawl.js
@@ -1,91 +1,49 @@
 const fs = require('fs')
-const request = require('request')
+const axios = require('axios')
 
 const curPage = 1
-const songNum = 20
-// const fileName = `${singerName}.json`
-
-// const url = `${HOST}/v8/fcg-bin/fcg_v8_toplist_cp.fcg${query}`;
+const songNum = 12
 
 function encode(str) {
     return encodeURIComponent(str)
 }
-function fetchData(name = '林俊杰') {
-    let url = `https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=sizer.yqq.song_next&searchid=151387985106450562&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=${curPage}&n=${songNum}&w=${encode(name)}&g_tk=162512859&jsonpCallback=searchCallbacksong8555&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
-    let requestOption = {
-      url: url,
-      method: 'GET'
-    }
-    let p = new Promise((resolve, reject) => {
-      request(requestOption, (err, res, body) => {
-          if (err) throw err
-          const ptn = /(\{.*\})/gi
-          const data = JSON.parse(body.match(ptn)[0])
-          getRealUrlArray(data, resolve)
-      })
-    })
-    // p.then((data) => {
-    //     console.log(data)
-    // })
-    return p
-
+async function request(url) {
+    const result = await axios.get(url)
+    const ptn = /(\{.*\})/gi
+    const data = JSON.parse(result.data.match(ptn)[0])
+    return data
 }
-function getRealUrlArray(data, resolve) {
-    // fs.writeFile(fileName, '[', (err) => {
-    //     if (err) throw err
-    //     console.log(fileName, 'is created')
-    // })
-
-    const songlist = data.data.song.list
-    const nameAndMids = songlist.map(v => {
+async function fetchData(name = '林俊杰') {
+    const url = `https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=sizer.yqq.song_next&searchid=151387985106450562&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=${curPage}&n=${songNum}&w=${encode(name)}&g_tk=162512859&jsonpCallback=searchCallbacksong8555&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
+    const data = await request(url)
+    const songs = await getSongsArray(data)
+    return songs
+}
+async function getSongsArray(data) {
+    const songList = data.data.song.list
+    const keySongs = songList.map(v => {
         return {
             singer: v.singer.map(singer => singer.name).join(' '),
             name: v.name,
             mid: v.mid
         }
     })
-    // resolve(nameAndMids)
-    // let song = nameAndMids[0]
-    // getSongUrl(song, resolve)
     const arr = []
-    const allPromise = []
-    for (const song of nameAndMids) {
-        const tmp = new Promise((resolve, reject) => {
-            getSongUrl(song, resolve)
-        }).then((data) => {
-            // resolve(data)
-            // console.log(data)
-            arr.push(data)
-        })
-        allPromise.push(tmp)
+    for (const song of keySongs) {
+        arr.push(await getOneSongUrl(song))
     }
-    Promise.all(allPromise).then(() => { 
-        // console.log(arr)
-        resolve(arr)
-    })
+    return arr
 }
-function getSongUrl(song, resolve) {
+async function getOneSongUrl(song) {
     const url = `https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg?songmid=${song.mid}&tpl=yqq_song_detail&format=jsonp&callback=getOneSongInfoCallback&g_tk=5381&jsonpCallback=getOneSongInfoCallback&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
-    const requestOption = {
-        url: url,
-        method: 'GET'
+    const data = await request(url)
+    const realUrl = Object.values(data.url)[0]
+    const one = {
+        name: song.name,
+        url: realUrl,
+        singer: song.singer
     }
-        
-    request(requestOption, (err, res, body) => {
-        if (err) throw err
-        const ptn = /(\{.*\})/gi
-        const data = JSON.parse(body.match(ptn)[0])
-        const realUrl = Object.values(data.url)[0]
-        const one = {
-            name: song.name,
-            url: realUrl,
-            singer: song.singer
-        }
-        resolve(one)
-        // fs.appendFile(fileName, `${JSON.stringify(one)},\n`, (err) => {
-        //     if (err) throw err
-        // })
-    })
+    return one
 }
 
 module.exports = fetchData
--- index.js
+++ index.js
@@ -7,40 +7,42 @@ const socket = require('socket.io')
 const io = socket(http)
 const crawl = require('./crawl')
 let curMusic = ''
+let usersNum = 0
 
 app.use(express.static(__dirname + '/statics'))
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile('index.html')
   // path.join(__dirname, 'public')
   //   res.sendFile(__dirname + '/statics/index.html')
 })
 
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
     console.log('a user connected')
+    usersNum++
+    socket.emit('onlineUser', usersNum)
+    console.log('the current number of online number:', usersNum)    
     if (curMusic) {
         socket.emit('changePlay', curMusic)
     }
-    socket.on('disconnect', function() {
-        console.log('user disconnected')
+
+    socket.on('disconnect', () => {
+        usersNum--
+        console.log('a user disconnected')
     })
 
-    socket.on('submit', function(name) {
-      // todo 添加爬虫代码 之后在emit回歌曲地址
-        crawl(name).then(data => {
-            // console.log(data)
-            // arr.push(data)
-            io.emit('change', data)
-        })
+    socket.on('submit', async (name) => {
+        const songList = await crawl(name)
+        io.emit('change', songList)
     })
-    socket.on('play', function(e) {
-        // console.log('111', e)
+
+    socket.on('play', (e) => {
         curMusic = e
         io.emit('changePlay', e)
     })
 })
 
 const port = process.env.HTTP_PORT || 3001 
-http.listen(port, function() {
+http.listen(port, () => {
     console.log('listening on *:' + port)
 })
