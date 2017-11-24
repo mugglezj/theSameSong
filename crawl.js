3c39b066aced3fcedfceae564eb89acd846646cc
lizimeow <lizimeowww@gmail.com>
2017-11-24 17:47:21 +0800

    优化代码结构 & 判断是否有版权

--- crawl.js
+++ crawl.js
@@ -7,43 +7,38 @@ const songNum = 12
 function encode(str) {
     return encodeURIComponent(str)
 }
-async function request(url) {
+
+async function fetchData(name = '孙燕姿') {
+    const url = `https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=sizer.yqq.song_next&searchid=151387985106450562&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=${curPage}&n=${songNum}&w=${encode(name)}&g_tk=162512859&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
     const result = await axios.get(url)
-    const ptn = /(\{.*\})/gi
-    const data = JSON.parse(result.data.match(ptn)[0])
-    return data
-}
-async function fetchData(name = '林俊杰') {
-    const url = `https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=sizer.yqq.song_next&searchid=151387985106450562&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=${curPage}&n=${songNum}&w=${encode(name)}&g_tk=162512859&jsonpCallback=searchCallbacksong8555&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
-    const data = await request(url)
-    const songs = await getSongsArray(data)
+    const songs = await getSongsArray(result.data)
     return songs
 }
+
 async function getSongsArray(data) {
     const songList = data.data.song.list
     const keySongs = songList.map(v => {
+        const url = v.pay.pay_play ? '' : getOneSongUrl(v.mid)
         return {
             singer: v.singer.map(singer => singer.name).join(' '),
             name: v.name,
-            mid: v.mid
+            url: url
         }
     })
-    const arr = []
-    for (const song of keySongs) {
-        arr.push(await getOneSongUrl(song))
-    }
-    return arr
+    const urls = await Promise.all(keySongs.map(v => v.url))
+    urls.forEach((v, i) => {
+        keySongs[i].url = urls[i]
+    })
+    // console.log(keySongs)
+    return keySongs
 }
-async function getOneSongUrl(song) {
-    const url = `https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg?songmid=${song.mid}&tpl=yqq_song_detail&format=jsonp&callback=getOneSongInfoCallback&g_tk=5381&jsonpCallback=getOneSongInfoCallback&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
-    const data = await request(url)
-    const realUrl = Object.values(data.url)[0]
-    const one = {
-        name: song.name,
-        url: realUrl,
-        singer: song.singer
-    }
-    return one
+
+async function getOneSongUrl(mid) {
+    const url = `https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg?songmid=${mid}&tpl=yqq_song_detail&format=json&callback=getOneSongInfoCallback&g_tk=5381&jsonpCallback=getOneSongInfoCallback&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0`
+    const result = await axios.get(url)
+    const [realUrl] = Object.values(result.data.url)
+    // const realUrl = Object.values(result.data.url)[0]
+    return realUrl
 }
 
 module.exports = fetchData
