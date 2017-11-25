f32bb7e33f428a4763a0b17131c5bd5051aa330b
zj <zj6@meitu.com>
2017-11-26 01:48:15 +0800

    genius zj

--- test/qqCrawl.test.js
+++ test/qqCrawl.test.js
@@ -1,3 +1,14 @@
 /**
  * Created by zj-db0818 on 2017/11/26.
  */
+const qqCrawl = require('../crawl/qqCrawl')
+
+let qq = new qqCrawl(1,10);
+(async function () {
+    let data = await qq.fetchData('张杰')
+    if (data.length === 10) {
+        console.log('success')
+    } else {
+        console.error('error')
+    }
+})()
